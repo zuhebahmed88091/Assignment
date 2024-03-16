ffa7e7eb274ab4de584160db1d75e1586307baa0
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2024-03-16 23:07:38 +0600

    Created Login form template

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,9 +1,10 @@
+import Login from "./Login";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Login />
     </div>
   );
 }
--- Frontend/frontend/src/Login.js
+++ Frontend/frontend/src/Login.js
@@ -1,5 +1,21 @@
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="d-flex justify-content-center align-items-center bg-black vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <form action="">
+          <div className="mb-3">
+            <label htmlFor="email"><strong>Email</strong></label>
+            <input type="email" placeholder="Enter Email" className="form-control rounded-0" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password"><strong>Password</strong></label>
+            <input type="password" placeholder="Enter Password" className="form-control rounded-0" />
+          </div>
+          <button className="btn btn-success w-100 rounded-0">Log In</button>
+          <p>Don't have an account?</p>
+          <button className="btn btn-default border w-100 bg-light rounded-0">Create Account</button>
+        </form>
+      </div>
+    </div>
+  );
 }
