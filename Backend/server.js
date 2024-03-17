5726eafdcb9ce515b3a2470a622f2a2e3e7b5038
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2024-03-17 12:02:51 +0600

    Created Docker image

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
 
 app.post("/signup", (req, res) => {
   const sql = "INSERT INTO login (name, email, password) VALUES (?)";
-//   const sql = "INSERT INTO login (`name`, `email`, `password`) VALUES ('" + req.body.name + "', '" + req.body.email + "', '" + req.body.password + "')";
+  //   const sql = "INSERT INTO login (`name`, `email`, `password`) VALUES ('" + req.body.name + "', '" + req.body.email + "', '" + req.body.password + "')";
   const values = [req.body.name, req.body.email, req.body.password];
 
   db.query(sql, [values], (err, data) => {
@@ -27,19 +27,19 @@ app.post("/signup", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-    const sql = "SELECT * FROM login WHERE email = ? AND password = ?";
-  
-    db.query(sql, [req.body.email, req.body.password], (err, data) => {
-      if (err) {
-        return res.json("Error");
-      }
-      if(data.length > 0){
-        return res.json("Login Success");
-      } else {
-        return res.json("Failed");
-      }
-    });
+  const sql = "SELECT * FROM login WHERE email = ? AND password = ?";
+
+  db.query(sql, [req.body.email, req.body.password], (err, data) => {
+    if (err) {
+      return res.json("Error");
+    }
+    if (data.length > 0) {
+      return res.json("Login Success");
+    } else {
+      return res.json("Failed");
+    }
   });
+});
 
 app.listen(8801, () => {
   console.log("running server");
